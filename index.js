0271f19b931ba65ad5eb8e8e10c19388f83454a6
inokappa <inokara@gmail.com>
2019-07-09 23:29:10 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const opener = require("opener");
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.hooks = {
+      'after:deploy:deploy': this.afterDeployTestament.bind(this),
+    };
+  }
+
+  afterDeployTestament() {
+    this.serverless.cli.log(this.serverless.service.custom.testament.message);
+    opener(this.serverless.service.custom.testament.url);
+  }
+}
+
+module.exports = ServerlessPlugin;
